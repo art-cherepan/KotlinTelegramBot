aec01abdde38dbdbcb1460b3c53348566fa16731
Artem Cherepanov <cherepanov_aa@drom.ru>
2025-03-05 22:12:03 +0700

    Добавить возможность обрабатывать отрицательные chatId

--- src/main/kotlin/english_bot/Telegram.kt
+++ src/main/kotlin/english_bot/Telegram.kt
@@ -8,7 +8,7 @@ fun main(args: Array<String>) {
     var updateId = 0
     val messageTextRegex: Regex = "\"text\":\"(.+?)\"".toRegex()
     val updateIdRegex: Regex = "\"update_id\":(\\d+)".toRegex()
-    val chatIdRegex: Regex = "\"chat\":\\{\"id\":(\\d+)".toRegex()
+    val chatIdRegex: Regex = "\"chat\":\\{\"id\":(-*\\d+)".toRegex()
     val dataRegex: Regex = "\"data\":\"(.+?)\"".toRegex()
 
     val telegramBotService = TelegramBotService(botToken = args[0])
